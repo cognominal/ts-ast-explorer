62cdca959499f2d9a92cbbfadf4b242b428d4ff2
Stéphane Payrard <cognominal@gmail.com>
2023-11-20 20:55:22 +0100

    add the option updateASTOnSaveOnly, not used yet

--- src/extension.ts
+++ src/extension.ts
@@ -8,8 +8,15 @@ import { initTreeview } from './treeviewAST';
 // The second one is more modular but for a small extension it is unnecessary
 // but it may hide dependencies if any
 
+export let updateASTOnSaveOnlyConfigOption: boolean;
+
+
 export function activate(context: vscode.ExtensionContext) {
 	initTreeview(context)
+    let config = vscode.workspace.getConfiguration('ts-ast-explorer');
+    updateASTOnSaveOnlyConfigOption = config.get('updateASTOnSaveOnly', true);
+
+
 }
 
 // This method is called when your extension is deactivated
