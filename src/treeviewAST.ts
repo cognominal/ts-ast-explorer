1b91fdea35eb671920d5e6bd49047626ce09a807
Stéphane Payrard <cognominal@gmail.com>
2023-11-25 04:43:23 +0100

    cleaning treeviewAST.ts

--- src/treeviewAST.ts
+++ src/treeviewAST.ts
@@ -1,20 +1,6 @@
 import * as vscode from 'vscode';
 import ts from 'typescript';
-import { getEnumBitFlags, EnumType } from './utils';
-import { compile } from './utils';
-
-
-// let f = ts.factory
-// let block: ts.Block = f.createBlock([f.createExpressionStatement(f.createNumericLiteral(1))]);
-
-// let code = ` let a =1`
-
-// // Compile the source code to AST
-// let ast = ts.createSourceFile('temp.ts', code, ts.ScriptTarget.Latest, true, ts.ScriptKind.TS);
-
-// // TODO: Use the AST for further processing
-
-
+import { getEnumBitFlags } from './utils';
 
 export function initTreeview(context: vscode.ExtensionContext) {
     const astProvider = new ASTProvider();
@@ -23,14 +9,6 @@ export function initTreeview(context: vscode.ExtensionContext) {
 
 
 }
-// let treeView = vscode.window.createTreeView('yourTreeViewId', {
-//     treeDataProvider: yourTreeDataProvider
-// });
-
-// let itemToSelect = yourTreeDataProvider.getTreeItem(yourDesiredNode);
-
-// treeView.reveal(itemToSelect, { select: true, focus: true });
-
 
 
 export class ASTProvider implements vscode.TreeDataProvider<ASTItem> {
@@ -77,38 +55,8 @@ export class ASTProvider implements vscode.TreeDataProvider<ASTItem> {
         // console.log(items)
         return items;
     }
-
-    // _getChildren(element?: ASTItem | undefined): vscode.ProviderResult<ASTItem[]> {
-    // //     function handleKey(key: string, element: ASTItem) {
-    //         let val = element.astNode[key as keyof ts.Node];
-    //         let str = typeof val === 'object' ? key : stringifyItem(key, val);
-    //         let collapsible = vscode.TreeItemCollapsibleState.None
-    //         if (typeof val === 'object') { collapsible = vscode.TreeItemCollapsibleState.Expanded }
-    //         let item = new ASTItem(str, collapsible, val);
-    //         children.push(item)
-    //     }
-
-    //     if (!this.root) { return [] }
-    //     element = element || new ASTItem('root', vscode.TreeItemCollapsibleState.Expanded, this.root);
-    //     let children: ASTItem[] = [];
-    //     if (element.astNode!['kind']) {
-    //         handleKey('kind', element) // add the kind first
-    //     }
-    //     for (const key in element.astNode) {
-    //         if (element.astNode.hasOwnProperty(key)) {
-    //             let val = element.astNode[key as keyof ts.Node]; // Add type assertion to keyof ts.Node
-    //             if (val === undefined || ['pos', 'end', 'kind'].includes(key)) {
-    //                 continue
-    //             } // skip undefined values and some keys to avoid clutter
-    //             handleKey(key, element)
-    //         }
-    //     }
-    //     return children
-    // }
-    // getParent?(element: ASTItem): vscode.ProviderResult<ASTItem> {
-    //     throw new Error('Method not implemented.');
-    // }
 }
+
 export class ASTItem extends vscode.TreeItem {
     constructor(
         public readonly label: string,
